a8123722c7048819d3a4a944a2339e3e9a7367fb
andreskull2 <andreskull1@gmail.com>
2025-06-28 01:49:36 -0300

    Getting the entered value

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const amount = document.getElementById("amount")
+
+// Manipulando o input amount para receber somunte números.
+amount.addEventListener("input", () => {
+    console.log(amount.value)
+})
